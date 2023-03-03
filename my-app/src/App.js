55cc7bd8f4cd75be05c28816888f9a8e137bdab7
michaelvong <mvong006@ucr.edu>
2023-03-02 18:22:23 -0800

    Added spotify auth and displayed basic user details

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 function App() {
+  const authEndpoint = 'https://accounts.spotify.com/authorize';
+  const clientId = "e9e84e9d5c72484a96a42e5bc7d42512";
+  const redirectUri = "http://localhost:3000/";
+  const scopes = [
+    'user-read-private'
+  ];
+
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState(null);
+
+  //useEffect is called when page opens, this component strips the token from the hash
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+    setToken(token)
+
+}, [])
+
+  //this component grabs the user using spotify web api
+  useEffect(() => {
+    if (token) {
+      let spotifyApi = new SpotifyWebApi();
+      spotifyApi.setAccessToken(token);
+      spotifyApi.getMe().then((r) => {
+        setUser(r);
+      });
+    }
+  }, [token])
+
+  //login component, redirects to spotify auth page
+  const handleLogin = () => {
+    window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+  }
+
+
+  //basic logout component, removes token and refreshes page
+  const handleLogout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+    window.location.reload(false)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Spotify Project</h1>
+     <button type="login" onClick={handleLogin}>Login</button>
+     
+     { user !== null && 
+      <div>
+        <h2>test{user.display_name}</h2>
+        <h2>{user.id}</h2>
+        <h3>Followers: {user.followers.total}</h3>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+     }
+     
     </div>
   );
 }
